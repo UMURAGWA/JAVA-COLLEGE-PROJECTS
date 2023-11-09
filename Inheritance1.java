177952517a9aee44eedcf72458ca1423fb14ebce
UMURAGWA <138700509+UMURAGWA@users.noreply.github.com>
2023-11-09 11:03:04 +0200

    Update Inheritance 1

--- Inheritance1.java
+++ Inheritance1.java
@@ -10,7 +10,7 @@ class D implements E {
     }
 }
 
-// Class C extending D and implementing the E interface
+// Class C extending D 
 class C extends D {
     public void play() {
         System.out.println("Class C: Goalkeeper - Blocks shots and clears the ball.");
@@ -19,27 +19,27 @@ class C extends D {
 
 // Class B extending C
 class B extends C {
-    void specificMethodB() {
-        System.out.println("Class B: Additional specific method.");
+    void play1() {
+        System.out.println("Class B: Midfielder- Control the ball and play making.");
     }
 }
 
-// Class A extending B and implementing the E interface
-class A extends B implements E {
+// Class A extending B 
+class A extends B {
     public void play() {
-        System.out.println("Class A: Forward - Scores goals and attacks opponents' defense.");
+        System.out.println("Class A: Forward - Scores goals and attacks opponents defense.");
     }
 }
 
 public class Inheritance1{
     public static void main(String[] args) {
         A playerA = new A();
-        E playerB = new D(); // You can create another player of the same type
-        D playerC = new A();
+        E playerB = new D(); 
+        B playerC = new B();
         D playerD = new C();
         playerA.play(); // Calls the play method in class A
         playerB.play(); // Calls the play method in class D
-        playerC.play(); // Calls the play method in class A
-        playerD.play(); // Calls the play method in class A
+        playerC.play1(); // Calls the play method in class B
+        playerD.play(); // Calls the play method in class C
     }
 }
